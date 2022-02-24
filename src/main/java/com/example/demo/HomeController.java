587c4b025c2872a936a19aef7cc987e854630420
rex7 <regischarles7@gmail.com>
2022-02-24 13:20:52 +0530

    second commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 	
-	@GetMapping("/home")
-	public String home() {
-		return "hello wworld from spring boot";
+	@RequestMapping("/home")
+	public ModelAndView home() {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("index.jsp");
+		return mv;
 	} 
 
 }
